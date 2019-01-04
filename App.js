b66216ec05af7becfa5908e7e6b5630417043480
Chris Harrop <chris@jigsaw.xyz>
2019-01-04 10:50:51 +0000

    Add logo and play with styling

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import logo from './assets/logo.jpg'
 
 class Sub extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<Text>Hello World!</Text>
-				<Button title="Little Button" onPress={() => {}}>Click Me</Button>
+				<Button title="Send!" onPress={() => {
+					alert('You clicked me!')
+				}}/>
 			</View>
 		);
+	}
+}
+
+class Header extends React.Component {
+	render() {
+		return (
+			<View style={styles.header}>
+				<Image source={logo} style={{width: 60, height: 60}} />
+			</View>
+
+		)
 	}
 }
 
 export default class App extends React.Component {
 	render() {
 		return (
-			<View style={styles.container}>
+			<View style={styles.page}>
+				<Header />
 				<Sub />
 			</View>
 		);
@@ -23,10 +38,19 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-	container: {
+	page: {
 		flex: 1,
+		marginTop: '10%',
+		padding: '5%',
 		backgroundColor: '#fff',
-		alignItems: 'center',
 		justifyContent: 'center',
 	},
+	header: {
+		flex: 1,
+		alignItems: 'flex-end',
+	},
+	container: {
+		flex: 1,
+		alignItems: 'center',
+	}
 });
