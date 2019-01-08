d26701859c767735f57e5227e847e9198a522c16
Chris Harrop <chris@jigsaw.xyz>
2019-01-08 11:00:52 +0000

    Add config for heroku deploy

--- server/public/javascript.js
+++ server/public/javascript.js
@@ -1,9 +1,7 @@
 const socket = io.connect('http://127.0.0.1:3000/');
 
 $(document).ready(function () {
-  console.log('loaded')
   $('form').submit((e) => {
-    console.log('form submitted')
     e.preventDefault();
     socket.emit('chat message', $('#m').val(), { user: 'Reach Admin' });
     $('#m').val('');
@@ -11,7 +9,6 @@ $(document).ready(function () {
   });
 
   socket.on('chat message', function(msg, props) {
-		console.log("​msg", msg)
     $('#messages').append($('<li>').text(`${props.user}: ${msg}`))
   })
 });
